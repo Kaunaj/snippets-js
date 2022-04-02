8881b0d88eab04266bc55aed36aa844e068ef2c6
Kaunaj Banerjee <kaunajbanerjee@Kaunajs-MacBook-Pro.local>
2022-04-02 20:14:07 +0530

    [add] express hello world template

---
+++ backend/express/hello-world.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const express = require("express");
+
+const ENV = process.argv[2] || process.env.NODE_ENV || "dev";
+if (ENV === "production") {
+    require("dotenv").config();
+} else {
+    require("dotenv").config({ path: path.join(__dirname, ".env-dev") });
+}
+
+const app = express();
+const port = process.env.PORT || 3000;
+const ipaddress = process.env.IP || "127.0.0.1";
+
+const logger = (req, res, next) => {
+    console.log(`[ ${new Date().toLocaleString()} ] ${req.method} ${res.statusCode} ${req.url}`);
+    next();
+};
+
+app.get("/", onRootHandler, logger);
+
+const server = app.listen(port, ipaddress, () => {
+    console.log(`Server started on ${ipaddress}:${port}`);
+});
+
+function onRootHandler(req, res, next) {
+    res.status(200).send("Hello world");
+    next();
+}
